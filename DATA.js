d4b1167c255f6a09ad3393c45476e60e22e96ba5
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:44:38 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "afa960ff-cc00-4870-97d6-de738da55ae6",
+    "home_feed": [
+        {
+            "id": "5ec879ed-7b85-4cde-b778-7a0aec4e90d5",
+            "artist_name": "moment",
+            "home_carousel": [
+                {
+                    "id": "c23b0f12-66b8-4ba2-8811-ca7e9aa9319a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "acadefd1-53df-42f5-a5eb-fb7961cb4e21",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                },
+                {
+                    "id": "86e1caa0-8fa9-4822-8031-a2e982a95822",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "e3df6429-293b-4fe5-a00c-5b315784e802",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "51236bad-9a6b-46fd-89fc-2a8d0460bd08",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                }
+            ],
+            "art_caption": "health",
+            "art_description": "Television century on visit decision pressure often truth. Open necessary open exist thank face law. Mind reduce lead quality professor particularly. Many trouble themselves skill start majority."
+        },
+        {
+            "id": "44a59979-3cc2-4bf1-8081-69c62e3352f8",
+            "artist_name": "by",
+            "home_carousel": [
+                {
+                    "id": "524957b2-a753-4b9a-9213-c1a626feda1f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "516d795e-9d14-4c47-be24-dd45f0f01861",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "433607d9-5928-4fe0-9a2f-2c541c77b699",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "216c3159-384f-4b45-9c02-d31aeeb97b26",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "2b59ed8f-4b07-4b16-84fd-ab92ef14e5f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                }
+            ],
+            "art_caption": "hair",
+            "art_description": "Check idea thought. He class with bit."
+        },
+        {
+            "id": "ba117c14-0be8-4c27-a162-2cd60ace0780",
+            "artist_name": "candidate",
+            "home_carousel": [
+                {
+                    "id": "0235b42c-2b87-4e43-8ecd-8e28b3d935bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/342.png"
+                },
+                {
+                    "id": "7f006d60-41d1-4e70-8c2e-c9ca09cde4dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "dc65d0f5-fd34-4921-8963-399aa9dee11b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "573e6e87-8f95-4fb2-9783-d8ad13772363",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "db313f69-13a9-4c61-a365-9282e99dbe1b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                }
+            ],
+            "art_caption": "friend",
+            "art_description": "Film detail magazine hundred just."
+        },
+        {
+            "id": "874b56c3-8ae5-4149-8c66-2e8fa7ef3ef2",
+            "artist_name": "western",
+            "home_carousel": [
+                {
+                    "id": "762391a7-cf76-4ef1-b296-d2541b6b1e90",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "7e4eebc7-ae9e-485f-ad4a-82f9ed3c6a93",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "8f966e47-62cf-43a9-818e-c5205580b940",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "2b18ba53-3ce5-4d7a-9367-4aa117a8a998",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "262404c6-30eb-4fc2-9b9a-2d839ec908bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                }
+            ],
+            "art_caption": "particularly",
+            "art_description": "No whose thousand accept suffer."
+        },
+        {
+            "id": "5541a342-2f30-4f01-adeb-ed06da26f7dd",
+            "artist_name": "tree",
+            "home_carousel": [
+                {
+                    "id": "226b6cf1-3d4f-4616-a74c-361d0858f68c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+                },
+                {
+                    "id": "5e76cf80-4d59-46b6-85a4-358f771fdc8f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                },
+                {
+                    "id": "1784f6a2-1fe9-4434-9ce0-de4c539ad724",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png"
+                },
+                {
+                    "id": "d236ee2e-bedf-46d4-bef5-ff90effa4dc1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "02f6efb7-8f0e-4726-b2a4-f08e7fb62f60",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                }
+            ],
+            "art_caption": "little",
+            "art_description": "Democratic physical community soldier. Step require only thousand. Ever sure yourself present."
+        }
+    ]
+}
